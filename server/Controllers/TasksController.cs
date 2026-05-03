ea52bb05d43f15bac8411e173acfc537ce1a8dba
Patricia Moraru <patriciamoraruv@gmail.com>
2026-05-03 14:57:08 +0300

    feat: add TasksController with AppDbContext dependency injection

---
+++ server/Controllers/TasksController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FishyTodo.API.Data;
+using FishyTodo.API.Models;
+
+namespace FishyTodo.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TasksController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public TasksController(AppDbContext db)
+    {
+        _db = db;
+    }
+}
