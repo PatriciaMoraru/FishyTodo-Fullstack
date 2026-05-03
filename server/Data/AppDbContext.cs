e72c77b0f23227066b40f7a3905e9c9a8ce5d9d5
Patricia Moraru <patriciamoraruv@gmail.com>
2026-05-03 16:36:33 +0300

    refactor: use Guid for TaskItem identifier

--- server/Data/AppDbContext.cs
+++ server/Data/AppDbContext.cs
@@ -8,4 +8,13 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskItem>(e =>
+        {
+            e.HasKey(t => t.Id);
+            e.Property(t => t.Id).ValueGeneratedNever();
+        });
+    }
 }
